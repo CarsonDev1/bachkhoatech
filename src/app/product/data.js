755fa71f4482352ee275c09e96f64216862dbde4
TranLocBK <loc@bachkhoa.tech>
2025-07-23 16:52:05 +0700

    update: list used school customer

---
+++ src/app/product/data.js
@@ -0,0 +1,122 @@
+export const schools = [
+  {
+    id: 1,
+    image: "/images/logo-schools/1.png",
+  },
+  {
+    id: 2,
+    image: "/images/logo-schools/2.png",
+  },
+  {
+    id: 3,
+    image: "/images/logo-schools/3.png",
+  },
+  {
+    id: 4,
+    image: "/images/logo-schools/4.png",
+  },
+  {
+    id: 5,
+    image: "/images/logo-schools/5.png",
+  },
+  {
+    id: 6,
+    image: "/images/logo-schools/6.png",
+  },
+  {
+    id: 7,
+    image: "/images/logo-schools/7.png",
+  },
+  {
+    id: 8,
+    image: "/images/logo-schools/8.png",
+  },
+  {
+    id: 9,
+    image: "/images/logo-schools/9.png",
+  },
+  {
+    id: 10,
+    image: "/images/logo-schools/10.png",
+  },
+  {
+    id: 11,
+    image: "/images/logo-schools/11.png",
+  },
+  {
+    id: 12,
+    image: "/images/logo-schools/12.png",
+  },
+  {
+    id: 13,
+    image: "/images/logo-schools/13.png",
+  },
+  {
+    id: 14,
+    image: "/images/logo-schools/14.png",
+  },
+  {
+    id: 15,
+    image: "/images/logo-schools/15.png",
+  },
+  {
+    id: 16,
+    image: "/images/logo-schools/16.png",
+  },
+  {
+    id: 17,
+    image: "/images/logo-schools/17.png",
+  },
+  {
+    id: 18,
+    image: "/images/logo-schools/18.png",
+  },
+  {
+    id: 19,
+    image: "/images/logo-schools/19.png",
+  },
+  {
+    id: 20,
+    image: "/images/logo-schools/20.png",
+  },
+  {
+    id: 21,
+    image: "/images/logo-schools/21.png",
+  },
+  {
+    id: 22,
+    image: "/images/logo-schools/22.png",
+  },
+  {
+    id: 23,
+    image: "/images/logo-schools/23.png",
+  },
+  {
+    id: 24,
+    image: "/images/logo-schools/24.png",
+  },
+  {
+    id: 25,
+    image: "/images/logo-schools/25.png",
+  },
+  {
+    id: 26,
+    image: "/images/logo-schools/26.png",
+  },
+  {
+    id: 27,
+    image: "/images/logo-schools/27.png",
+  },
+  {
+    id: 28,
+    image: "/images/logo-schools/28.png",
+  },
+  {
+    id: 29,
+    image: "/images/logo-schools/29.png",
+  },
+  {
+    id: 30,
+    image: "/images/logo-schools/30.png",
+  },
+]
